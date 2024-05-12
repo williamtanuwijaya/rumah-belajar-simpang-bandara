6fb0d1d13763fa0dea41eef2c9c6eb67db442e4f
williamtanuwijaya <will.tw0807@gmail.com>
2024-05-12 22:27:46 +0700

    batal untuk daftar screen jadi pake gform

--- javascript/daftar.js
+++ javascript/daftar.js
@@ -1,12 +1,26 @@
-const instrumenSelect = document.getElementById('instrumen');
-const otherText = document.getElementById('otherText');
+// const instrumenSelect = document.getElementById('instrumen');
+// const otherText = document.getElementById('otherText');
 
-instrumenSelect.addEventListener('change', function () {
-  if (instrumenSelect.value === 'other') {
-    otherText.style.display = 'block';
+// instrumenSelect.addEventListener('change', function () {
+//   if (instrumenSelect.value === 'other') {
+//     otherText.style.display = 'block';
 
-    otherText.focus();
-  } else {
-    otherText.style.display = 'none';
-  }
+//     otherText.focus();
+//   } else {
+//     otherText.style.display = 'none';
+//   }
+// });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwC2l75axeHSqBF3AQm_RkSL8WV8rl91I2JcVEvujpvCE8RNgEoWutePPt-42TZsUBS/exec';
+
+const form = document.forms['daftar'];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => alert('Thank you! your form is submitted successfully'))
+    .then(() => {
+      window.location.reload();
+    })
+    .catch(error => console.error('Error!', error.message))
 });
