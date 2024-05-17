5883c5c86cb1be2ba47ce69462a38c0a6a76fe56
williamtanuwijaya <will.tw0807@gmail.com>
2024-05-17 12:13:42 +0700

    Update

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,5 @@
 const heroText = document.querySelector('.hero-text');
 
-heroText.style.opacity = 0;
-
 window.addEventListener('scroll', function () {
   if (window.scrollY > 100) {
     heroText.style.animationPlayState = 'running';
